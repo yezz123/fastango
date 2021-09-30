31ba7fdbfe6c26062fae8da009fa2c254f3d5e30
Yasser Tahiri <yasserth19@gmail.com>
2021-09-30 17:44:45 +0100

    ✨ Ver 1.0.0 (#4)
    * Bump version: 0.0.1 → 1.0.0
    
    * Bump Version
    
    * Fix Setup File

--- setup.py
+++ setup.py
@@ -1,28 +1,26 @@
 import setuptools
 
-ver = "1.0.0"
+version = "1.0.0"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="fastapi-lazy",
-    version=ver,
-    author="Yezz123",
+    version=version,
+    author="Yasser Tahiri",
     author_email="yasserth19@gmail.com",
     description="Utilities that you use in various projects made in FastAPI",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/yezz123/fastapi-lazy",
+    packages=setuptools.find_packages(),
     include_package_data=True,
-    project_urls={"Bug Tracker": "https://github.com/yezz123/fastapi-lazy/issues",},
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Unlicense",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["fastapi", "databases", "motor", "pyjwt", "aioredis==2.0.0"],
-    package_dir={"": "fastapi-lazy"},
-    packages=setuptools.find_packages(where="fastapi-lazy"),
     python_requires=">=3.7",
+    install_requires=["fastapi", "databases", "motor", "pyjwt", "aioredis==2.0.0"],
 )
