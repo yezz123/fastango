684121ce79740c94b578a85b718b17ef903f285a
Yasser Tahiri <yasserth19@gmail.com>
2021-10-09 13:21:40 +0100

    Bump version: 2.0.0 → 3.0.0

--- fastapi_lazy/__init__.py
+++ fastapi_lazy/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2.0.0"
+__version__ = "3.0.0"
 
 """
     fastapi-lazy - A Lazy package-starter for FastAPI applications.
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-__version__ = "2.0.0"
+__version__ = "3.0.0"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
