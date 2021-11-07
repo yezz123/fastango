5fb80d515e59b12b989030fa553fead8ca11325c
Yasser Tahiri <yasserth19@gmail.com>
2021-11-07 02:45:05 +0100

    chore: Fix Version Issue

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 import setuptools
 
-from fastapi_lazy import __version__
-
-__version__ = __version__
+__version__ = "1.2.5"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
