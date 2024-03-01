620c9b5c15bf69fa0934d8bcd6f221050ef8819b
Yasser Tahiri <yasserth19@gmail.com>
2024-03-01 03:30:02 +0100

    ✨ Start working on `Fastango`

---
+++ fastango/__init__.py
@@ -0,0 +1,10 @@
+"""
+As you create more complex FastAPI applications, you may find yourself frequently repeating the same dependencies in multiple related endpoints.
+
+A common question people have as they become more comfortable with FastAPI is how they can reduce the number of times they have to copy/paste the same dependency into related routes.
+
+Inspired by the `class-based view` in [Fastapi-utils](https://fastapi-utils.davidmontague.xyz/user-guide/class-based-views/).
+
+"""
+
+__version__ = "2.0.0"
