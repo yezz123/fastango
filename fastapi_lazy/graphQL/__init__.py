1eb3e19fc04785ab5d873d464dc54ae0dcc05386
Yezz123 <yasserth19@gmail.com>
2026-05-30 12:41:31 +0100

    Remove unused files and configurations, including .editorconfig, .gitignore, .pre-commit-config.yaml, LICENSE, Makefile, README.md, requirements files, setup.py, and various FastAPI-related modules. This cleanup prepares the repository for a fresh start.

--- fastapi_lazy/graphQL/__init__.py
+++
@@ -1,8 +0,0 @@
-from graphQL.auth import bearer, handler
-from graphQL.services import user
-
-__all__ = [
-    "bearer",
-    "handler",
-    "user",
-]
--- fastapi_lazy/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from fastapi_lazy.utils import hash, validator
-
-__all__ = [
-    "hash",
-    "validator",
-]
